a295b7d92f2af7efc2198c6e3bebc4bcc83ef459
tessambrown <tebr1887@colorado.edu>
2024-06-30 11:57:27 -0600

    Update wa11.js

--- js/wa11.js
+++ js/wa11.js
@@ -27,18 +27,19 @@ for (let i = 0; i < filenames.length; i++) {
     newImage.setAttribute('alt', altText[i]);
 
     /* Applying styles to thumbnail images */
-    newImage.style.width = '100px'; /* Adjust as needed */
-    newImage.style.height = 'auto';
-    newImage.style.cursor = 'pointer';
-    newImage.style.transition = 'transform 0.2s';
+    // newImage.style.width = '100px'; /* Adjust as needed */
+    // newImage.style.height = 'auto';
+    // newImage.style.cursor = 'pointer';
+    // newImage.style.transition = 'transform 0.2s';
 
-    newImage.addEventListener('mouseenter', () => {
-        newImage.style.transform = 'scale(1.1)';
-    });
+    //images were not working so this scales them from inside javascript
+    // newImage.addEventListener('mouseenter', () => {
+    //     newImage.style.transform = 'scale(1.1)';
+    // });
 
-    newImage.addEventListener('mouseleave', () => {
-        newImage.style.transform = 'scale(1)';
-    });
+    // newImage.addEventListener('mouseleave', () => {
+    //     newImage.style.transform = 'scale(1)';
+    // });
 
     thumbBar.appendChild(newImage);
 
