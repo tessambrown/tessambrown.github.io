f9ec6f2d5179c5268633d8977c9611aed3c79618
tessambrown <tebr1887@colorado.edu>
2024-07-01 11:06:01 -0600

    Update ica12.js

--- js/ica12.js
+++ js/ica12.js
@@ -1,34 +1,25 @@
 const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
 
-const newQuote = document.querySelector("js-new-quote")
-newQuote.addEventListener['click', getTriva]; 
+const newQuote = document.querySelector('.js-new-quote');
+newQuote.addEventListener('click', getTrivia); 
 
-// made it an async function so the await wouldn't create an error
-async function getTriva() 
-{
-    // console.log('test')
-    try 
-    {
-        const response = await fetch[endpoint];
-        if(!response.ok)
-        {
+async function getTrivia() {
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
             throw Error(response.statusText);
         }
         const json = await response.json();
-        // console.log(json);
-        displayTriva[json('question')];
-    }
-    catch(err)
-    {
+        displayTrivia(json.question);
+    } catch (err) {
         console.log(err);
-        alert['Failed to fetch new quote']; ``
+        alert('Failed to fetch new quote');
     }
 }
 
-function displayTriva(quote) 
-{
-    const triviaText = document.querySelector('js-quote-text')
+function displayTrivia(quote) {
+    const triviaText = document.querySelector('.js-quote-text');
     triviaText.textContent = quote; 
 }
 
-getTriva();
+getTrivia();
