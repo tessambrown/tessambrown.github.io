7f317e8b6da54741b39602f74535abb35a845d14
tessambrown <tebr1887@colorado.edu>
2024-07-01 10:40:39 -0600

    Update ica12.js

--- js/ica12.js
+++ js/ica12.js
@@ -1,6 +1,24 @@
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
 const newQuote = document.querySelector("js-new-quote")
 newQuote.addEventListener('click', getTriva); 
 
-function getTriva() {
-    console.log('test')
+function getTriva() 
+{
+    // console.log('test')
+    try 
+    {
+        const response = await fetch(endpoint)
+        if(!response.ok)
+        {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+    }
+    catch(err)
+    {
+        console.log(err);
+        alert['Failed to fetch new quote'];
+    }
 }
