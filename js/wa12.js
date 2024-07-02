8c2c165f20850fd4226a54cefe948090ff639118
tessambrown <tebr1887@colorado.edu>
2024-07-02 16:23:05 -0600

    updated wa12

--- js/wa12.js
+++ js/wa12.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const endpoint = 'https://www.beanpoems.com/api/random';
+    const endpoint = 'GET https://www.beanpoems.com/api/random';
 
     const newPoemButton = document.querySelector('#new-poem');
     newPoemButton.addEventListener('click', getPoem);
