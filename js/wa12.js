2bb2feb40b8a0f813b26574c38bc706d9eec454d
tessambrown <tebr1887@colorado.edu>
2024-07-02 17:51:29 -0600

    Update wa12.js

--- js/wa12.js
+++ js/wa12.js
@@ -3,38 +3,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const endpoint = "https://api.quotable.io/quotes/random?limit=1";
 
     const newPoemButton = document.querySelector('#new-poem');
-    newPoemButton.addEventListener('click', getPoem);
+    newPoemButton.addEventListener('click', fetchRandomQuote);
 
-    async function getPoem() {
-        try
-        {
-            const poem = await fetch(endpoint);
+    // async function getPoem() {
+    //     try
+    //     {
+    //         const poem = await fetch(endpoint);
 
-            if(!poem.ok)
-            {
-                throw Error(poem.statusText);
-            }
+    //         if(!poem.ok)
+    //         {
+    //             throw Error(poem.statusText);
+    //         }
 
-            const json = await poem.json();
-            displayPoem(json);
-            clearPoem();
-        }
-        catch(err)
-        {
-            console.log('Fetch error', err);
-            alert('Failed to fetch new poem');
-        }
+    //         const json = await poem.json();
+    //         displayPoem(json);
+    //         clearPoem();
+    //     }
+    //     catch(err)
+    //     {
+    //         console.log('Fetch error', err);
+    //         alert('Failed to fetch new poem');
+    //     }
+    // }
+
+    function fetchRandomQuote() {
+        fetch('https://api.quotable.io/random')
+            .then(response => response.json())
+            .then(data => {
+                const quote = data.content;
+                document.getElementById("quote").textContent = quote;
+            })
+            .catch(error => {
+                console.error('Error fetching quote:', error);
+            });
     }
 
-    function displayPoem(quote)
-    {
-        const poemText = document.querySelector('#poem-text');
-        poemText.textContent = quote;
-    }
+    fetchRandomQuote();
 
-    function clearPoem()
-    {
-        const poemText = document.querySelector('#poem-text');
-        poemText.textContent = '';
-    }
+    // function displayPoem(quote)
+    // {
+    //     const poemText = document.querySelector('#poem-text');
+    //     poemText.textContent = quote;
+    // }
+
+    // function clearPoem()
+    // {
+    //     const poemText = document.querySelector('#poem-text');
+    //     poemText.textContent = '';
+    // }
 });
