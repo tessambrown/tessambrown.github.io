6483f61f3a47defb0d553b73b35b4ca7738788c1
tessambrown <tebr1887@colorado.edu>
2024-07-02 15:52:25 -0600

    updated wa12

--- js/wa12.js
+++ js/wa12.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const newPoemButton = document.querySelector('#new-poem');
     newPoemButton.addEventListener('click', getPoem);
 
+    newPoemButton.addEventListener('mouseenter', () => {
+        newPoemButton.style.transform = 'scale(1.1)';
+    });
+
+    newPoemButton.addEventListener('mouseleave', () => {
+        newPoemButton.style.transform = 'scale(1)';
+    });
+
     async function getPoem() {
         try
         {
@@ -11,12 +19,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if(!poem.ok)
             {
-                
+                throw Error(poem.statusText);
             }
+
+            const json = await poem.json();
+            displayPoem(json);
+            clearPoem();
         }
         catch(err)
         {
-
+            console.log('Fetch error', err);
+            alert('Failed to fetch new poem');
         }
+    }
+
+    function displayPoem(quote)
+    {
+        const poemText = document.querySelector('#poem-text');
+        poemText.textContent = quote;
+    }
+
+    function clearPoem()
+    {
+        const poemText = document.querySelector('#poem-text');
+        poemText.textContent = '';
     }
 });
