56069100fd851ad36d61a1b7582da41175748380
tessambrown <tebr1887@colorado.edu>
2024-07-02 21:51:38 -0600

    updated project 3

--- project3/project3.js
+++ project3/project3.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    // button 1
-
+    // display random letters on each button
     // button 2 -> A, B, C
     
-
     // button 3 -> D, E, F
 
+    // button 4 -> G, H, I
 
+    // button 5 -> J, K, L
+
+    // button 6 -> M, N, O
+
+    // button 7 -> P, Q, R, S
+
+    // button 8 -> T, U, V
+
+    // button 9 -> W, X, Y, Z
+
+    // if the buttons get pushed fill in the phone number
+    if(document.querySelector('#1'))
+    {
+
+    }
+
+    if(document.querySelector('#2'))
+    {
+
+    }
+    
 });
