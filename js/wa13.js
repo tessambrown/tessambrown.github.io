218d54d4f63289b75cd04bcba7b0a7928cfcfed8
tessambrown <tebr1887@colorado.edu>
2024-07-03 13:31:15 -0600

    updated wa13

--- js/wa13.js
+++ js/wa13.js
@@ -1,3 +1,25 @@
-async function populate() {
-    const requestURL = ""
+// problem 1:
+
+const problem1 = document.querySelector('#problem1');
+const requestURL1 = "https://tessambrown.github.io/wa/wa13.JSON";
+const requestURL2 = ""
+
+
+fetch(requestURL1)
+  .then(response => response.text())
+  .then(text => displayHRInfo(text));
+
+let employeeInfo = 'The employees are: ';
+
+async function displayHRInfo(HRstring) {
+  let employees = JSON.parse(HRstring);
+
+  employees.forEach(employee => {
+    let para = document.createElement('p');
+    para.textContent = `Name: ${employee.name}, Department: ${employee.department}, Designation: ${employee.designation}, Salary: ${employee.salary}, Raise Eligible: ${employee.raise ? 'Yes' : 'No'}`;
+    problem1.appendChild(para);
+  });
 }
+
+// problem 2:
+
